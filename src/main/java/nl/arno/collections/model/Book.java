bd228f4ca21a133ac2e09f9dd09648d8ff709b9d
Arno Laurman <arno.laurman@gmail.com>
2018-12-07 08:54:12 +0100

    Initial commit

--- src/main/java/nl/arno/collections/model/Book.java
+++ src/main/java/nl/arno/collections/model/Book.java
@@ -3,13 +3,17 @@ package nl.arno.collections.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+
 @Getter
 @Setter
+@Entity
 public class Book {
     private String isbn;
     private String title;
     private String author;
     private String genre;
     private boolean owned;
+    private String stored;
 
 }
--- src/test/java/nl/arno/collections/CollectionsApplicationTests.java
+++ src/test/java/nl/arno/collections/CollectionsApplicationTests.java
@@ -9,6 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CollectionsApplicationTests {
 
+	/**
+	 * First test
+	 */
 	@Test
 	public void contextLoads() {
 	}
